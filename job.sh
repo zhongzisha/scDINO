5184d6f0b0e94feebc2e5d2a5f5568739dc16cc5
zhongzisha <zhongzisha@outlook.com>
2024-11-02 17:09:00 -0400

    testing

--- job.sh
+++ job.sh
@@ -4,18 +4,38 @@ module load cuDNN/8.9.2/CUDA-12
 module load gcc/11.3.0
 
 
+source $FRCE_DATA_ROOT/anaconda3/bin/activate th23
+module load cuda/11.8
+module load cudnn/8.8.3-cuda11
 
 
 
-
-snakemake -s full_pipeline_snakefile1 all \
---configfile="configs/scDINO_full_pipeline1.yaml" \
+snakemake -s full_pipeline_snakefile1_biowulf all \
+--configfile="configs/scDINO_full_pipeline1_biofulf.yaml" \
 --keep-incomplete \
 --drop-metadata \
 --cores 8 \
 --jobs 40 \
 -k \
 --cluster "sbatch --time=24:00:00 --partition=gpu --gres=gpu:v100x:2 --ntasks=8 --mem=64G --cpus-per-task=16 --output=slurm_output.txt --error=slurm_error.txt" \
+--latency-wait 45
+
+
+snakemake -s full_pipeline_snakefile1 all \
+--configfile="configs/scDINO_full_pipeline1.yaml" \
+--keep-incomplete \
+--drop-metadata \
+--cores 32 \
+--jobs 1 \
+-k \
+--latency-wait 45
+
+snakemake -s full_pipeline_snakefile1_4gpus all \
+--configfile="configs/scDINO_full_pipeline1_4gpus.yaml" \
+--keep-incomplete \
+--drop-metadata \
+--cores 32 \
+-k \
 --latency-wait 45
 
 snakemake -s only_downstream_snakefile1 all \
